69aa6830fcfadc8263b94c895792ba52d23e34d2
J0hn316 <jcordell316@gmail.com>
2024-01-01 23:24:07 -0500

    next push

--- scripts/math/utils.js
+++ scripts/math/utils.js
@@ -10,9 +10,29 @@ function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
   }
   return nearest;
 }
+function getNearestSegment(loc, segments, threshold = Number.MAX_SAFE_INTEGER) {
+  let minDist = Number.MAX_SAFE_INTEGER;
+  let nearest = null;
+  for (const seg of segments) {
+    const dist = seg.distanceToPoint(loc);
+    if (dist < minDist && dist < threshold) {
+      minDist = dist;
+      nearest = seg;
+    }
+  }
+  return nearest;
+}
 
 function distance(p1, p2) {
   return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+}
+
+function average(p1, p2) {
+  return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+}
+
+function dot(p1, p2) {
+  return p1.x * p2.x + p1.y * p2.y;
 }
 
 function add(p1, p2) {
@@ -27,11 +47,23 @@ function scale(p, scaler) {
   return new Point(p.x * scaler, p.y * scaler);
 }
 
+function normalize(p) {
+  return scale(p, 1 / magnitude(p));
+}
+
+function magnitude(p) {
+  return Math.hypot(p.x, p.y);
+}
+
 function translate(loc, angle, offset) {
   return new Point(
     loc.x + Math.cos(angle) * offset,
     loc.y + Math.sin(angle) * offset
   );
+}
+
+function perpendicular(p) {
+  return new Point(-p.y, p.x);
 }
 
 function angle(p) {
@@ -43,7 +75,8 @@ function getIntersection(A, B, C, D) {
   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
   const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
-  if (bottom != 0) {
+  const eps = 0.001;
+  if (Math.abs(bottom) > eps) {
     const t = tTop / bottom;
     const u = uTop / bottom;
     if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
@@ -60,6 +93,10 @@ function getIntersection(A, B, C, D) {
 
 function lerp(a, b, t) {
   return a + (b - a) * t;
+}
+
+function lerp2D(A, B, t) {
+  return new Point(lerp(A.x, B.x, t), lerp(A.y, B.y, t));
 }
 
 function getRandomColor() {
--- scripts/primitives/polygon.js
+++ scripts/primitives/polygon.js
@@ -7,6 +7,40 @@ class Polygon {
     }
   }
 
+  static load(info) {
+    return new Polygon(info.points.map((i) => new Point(i.x, i.y)));
+  }
+
+  static union(polys) {
+    Polygon.multiBreak(polys);
+    const keptSegments = [];
+    for (let i = 0; i < polys.length; i++) {
+      for (const seg of polys[i].segments) {
+        let keep = true;
+        for (let j = 0; j < polys.length; j++) {
+          if (i != j) {
+            if (polys[j].containsSegment(seg)) {
+              keep = false;
+              break;
+            }
+          }
+        }
+        if (keep) {
+          keptSegments.push(seg);
+        }
+      }
+    }
+    return keptSegments;
+  }
+
+  static multiBreak(polys) {
+    for (let i = 0; i < polys.length - 1; i++) {
+      for (let j = i + 1; j < polys.length; j++) {
+        Polygon.break(polys[i], polys[j]);
+      }
+    }
+  }
+
   static break(poly1, poly2) {
     const segs1 = poly1.segments;
     const segs2 = poly2.segments;
@@ -31,6 +65,42 @@ class Polygon {
         }
       }
     }
+  }
+
+  distanceToPoint(point) {
+    return Math.min(...this.segments.map((s) => s.distanceToPoint(point)));
+  }
+
+  distanceToPoly(poly) {
+    return Math.min(...this.points.map((p) => poly.distanceToPoint(p)));
+  }
+
+  intersectsPoly(poly) {
+    for (let s1 of this.segments) {
+      for (let s2 of poly.segments) {
+        if (getIntersection(s1.p1, s1.p2, s2.p1, s2.p2)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  containsSegment(seg) {
+    const midpoint = average(seg.p1, seg.p2);
+    return this.containsPoint(midpoint);
+  }
+
+  containsPoint(point) {
+    const outerPoint = new Point(-1000, -1000);
+    let intersectionCount = 0;
+    for (const seg of this.segments) {
+      const int = getIntersection(outerPoint, point, seg.p1, seg.p2);
+      if (int) {
+        intersectionCount++;
+      }
+    }
+    return intersectionCount % 2 == 1;
   }
 
   drawSegments(ctx) {
